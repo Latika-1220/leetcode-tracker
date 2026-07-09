b44b61c2977f7396c416514bf58c45150bbf93a7
Latika S <latikasugavaneswaran20@gmail.com>
2026-07-09 09:55:00 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 09:55:02
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0)
+        {
+            return false;
+        }
+        else if(n==1||n==2){
+            return true;
+        }
+        else{
+            return true;
+        }
+    }
+}
