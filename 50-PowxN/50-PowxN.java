b1944732c9fd6372178078f4928239c730187502
Latika S <latikasugavaneswaran20@gmail.com>
2026-07-09 09:56:33 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,9 @@
+// Last updated: 09/07/2026, 09:56:36
+class Solution {
+    public double myPow(double x, int n) {
+        for(int i=0;i<n;i++){
+            return Math.pow(x,n);
+        }
+        return Math.pow(x,n);
+    }
+}
