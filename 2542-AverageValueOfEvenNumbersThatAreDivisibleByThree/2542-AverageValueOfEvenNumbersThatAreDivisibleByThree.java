13af3fb9723d8efb6a5814d49bddd2224a77725d
Latika S <latikasugavaneswaran20@gmail.com>
2026-07-09 09:53:02 +0530

    Create 2542-AverageValueOfEvenNumbersThatAreDivisibleByThree

---
+++ 2542-AverageValueOfEvenNumbersThatAreDivisibleByThree/2542-AverageValueOfEvenNumbersThatAreDivisibleByThree.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:53:04
+class Solution {
+    public int averageValue(int[] nums) {
+        int sum =0;
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                if(nums[i]%3==0){
+                    sum += nums[i];
+                    count++;
+                }
+            }
+        }
+        if(count==0){
+            return 0;
+        }
+        int avg = sum/count;
+        return avg;
+    }
+}
