ad5fc929e5606901cf1fad875cb590f938092ae9
Latika S <latikasugavaneswaran20@gmail.com>
2026-07-09 09:52:42 +0530

    Create 2857-TotalDistanceTraveled

---
+++ 2857-TotalDistanceTraveled/2857-TotalDistanceTraveled.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 09:52:44
+class Solution {
+    public int distanceTraveled(int mainTank, int additionalTank) {
+       int d=0;
+       while(mainTank>=5){
+        d+=50;
+        mainTank-=5;
+        if(additionalTank>0){
+            mainTank+=1;
+            additionalTank--;
+        }
+       }
+       d+=mainTank*10;
+       return d;
+    }
+}
