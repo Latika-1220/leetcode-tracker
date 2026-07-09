70154434302d1636f69855ab2f6dafefcd38943b
Latika S <latikasugavaneswaran20@gmail.com>
2026-07-09 09:53:43 +0530

    Create 1236-NThTribonacciNumber

---
+++ 1236-NThTribonacciNumber/1236-NThTribonacciNumber.java
@@ -0,0 +1,21 @@
+// Last updated: 09/07/2026, 09:53:45
+class Solution {
+    public int tribonacci(int n) {
+       if(n==0){
+        return 0;
+       }else if(n==1||n==2){
+        return 1;
+       }
+       int zero=0;
+       int first=1;
+       int second=1;
+       for(int i=3;i<=n;i++){
+        int tempnext=zero+first+second;
+        zero=first;
+        first=second;
+        second=tempnext;
+       }
+       return second;
+
+    }
+}
